d8f864e18d5990b01f55859af89cd784bc519488
AngelEgito <angelegito@hotmail.com>
2021-04-28 23:57:21 -0300

    definir fim do jogo

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "dow") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[1].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
 
